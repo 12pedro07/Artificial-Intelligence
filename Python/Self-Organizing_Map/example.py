09e06568f7137538919b72d55d534e8596a141e2
Pedro Domingues <12pedro07@gmail.com>
2020-03-24 15:17:03 -0300

    now plot results

--- Python/Self-Organizing_Map/example.py
+++ Python/Self-Organizing_Map/example.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from som import SOM
 
 inputs  = [[1,4],
@@ -17,4 +18,29 @@ learning_rate = 1
 sigma_decrease_rate = 0
 
 som = SOM(inputs, weights, sigma=sigma, learning_rate=learning_rate, sigma_decrease_rate=sigma_decrease_rate)
-som.train()
+som.train(ignore_hk=True, display=True)
+points = [[1,4],
+          [6,2],
+          [1,3],
+          [5,1],
+          [4,0],
+          [0,4],
+          [3,4]]
+
+predictions = som.predict(points, ignore_hk=True, display=True)
+
+p, m = som.prediction_line(display=True)
+
+def med(points, p, m):
+    y=[]
+    for point in points:
+        y.append(m*(point - p[0]) + p[1])
+    return y
+
+plt.grid()
+for point, prediction in zip(points, predictions):
+    x, y = point
+    color, label = ("blue", "cluster 1") if prediction[0] == 1 else ("red", "cluster 2")
+    plt.scatter(x,y,c=color,label=label,s=30)
+plt.plot(range(6), med(range(6), p, m))
+plt.show()
--- Python/Self-Organizing_Map/som.py
+++ Python/Self-Organizing_Map/som.py
@@ -1,5 +1,6 @@
 from math import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 class SOM():
     def __init__(self, inputs, weights, sigma=1, learning_rate=1, sigma_decrease_rate=0):
@@ -35,31 +36,73 @@ class SOM():
         coeficient = coeficient/square_base
         e = 1/exp(coeficient)
         #e = exp(-((delta)**2)/(2*(self.sigma**2)))
+        print("para {} e {}".format(wk, wj))
+        print("\te: ", e)
+        print("\tSigma: ",self.sigma)
         self.sigma -= self.sigma_decrease_rate
         return e
 
-    def delta_w(self,k,x):
+    def delta_w(self,k,x,ignore_hk=False):
         # k = winner idx
         # x = input
         delta_w = np.zeros(self.w.shape)
         for j in range(len(self.w)):
             for idx in range(len(self.w[j])):
-                delta_w[j][idx] = self.eta * self.hk(self.w[k][idx], self.w[j][idx]) * (x[idx] - self.w[k][idx])
+                if ignore_hk:
+                    if k == j:
+                        delta_w[j][idx] = self.eta * (x[idx] - self.w[k][idx])
+                else:
+                    delta_w[j][idx] = self.eta * self.hk(self.w[k][idx], self.w[j][idx]) * (x[idx] - self.w[k][idx])
         return delta_w
 
     def find_k(self, x):
+        # k = winner idx
         s = [self._euc_dist(x, weight) for weight in self.w]
         return np.argmin(s)
 
-    def train(self):
+    def train(self, ignore_hk=False, display=False):
         np.set_printoptions(precision = 2, suppress=True)
         for iteration, x in enumerate(self.inputs):
-            print("="*50)
-            print("ITERATION <{}>".format(iteration+1))
-            print("w = \n{}\n".format(self.w))
-            print("input = \n{}\n".format(x))
-            delta_w = self.delta_w(self.find_k(x),x)
-            print("delta w = \n{}\n".format(delta_w))
+            delta_w = self.delta_w(self.find_k(x),x, ignore_hk)
+            if display:
+                print("="*50)
+                print("ITERATION <{}>".format(iteration+1))
+                print("w = \n{}\n".format(self.w))
+                print("input = \n{}\n".format(x))
+                print("delta w = \n{}\n".format(delta_w))
+                print("new w = \n{}\n".format(delta_w + self.w))
             self.w = delta_w + self.w
-            print("new w = \n{}\n".format(self.w))
+
+    def linear2matrix(self,linear,matrix):
+        row = int(linear/len(matrix))
+        col = int(linear%len(matrix))
+        return (row, col)
+
+    def _med(self,p1,p2):
+        # median point
+        M  = ( (p1[0] + p2[0])/2, (p1[1] + p2[1])/2 )
+        # angular coeficient of (p1,p2).
+        m1 = (p1[1]-p2[1])/(p1[0]-p2[0])
+        # angular coeficient of the perpendicular line of (p1,p2) that pass throught the median
+        mM = -1/m1
+        # y - My = mM * (x - Mx)
+        return (M,mM)
+
+    def prediction_line(self,display=False):
+        # works only if there is 2 neurons
+        p, m = self._med(self.w[0],self.w[1])
+        print( "y - {} = {} * (x - {})".format(p[1],m,p[0]) )
+        print( "---> y = ({})*x + ({})".format(m,m*p[0]-p[1]))
+        return (p,m)
+
+    def predict(self, inputs, ignore_hk=False, display=False):
+        neurons = []
+        for x in inputs:
+            x = np.array(x)
+            neuron = [self._euc_dist(x,weight) for weight in self.w]
+            idx = neuron.index(max(neuron))
+            neuron = [1 if i==idx else 0 for i in range(len(neuron))]
+            neurons.append(neuron)
+            if display: print( "{} - {}".format(x,neuron) )
+        return neurons
 
